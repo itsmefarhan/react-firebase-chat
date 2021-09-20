4cd7569a47fef387445ad40fe81aa93f6fe3b443
Farhan Farooq <farhan.farooq@live.com>
2021-09-21 02:37:04 +0500

    profile upload image

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +21,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Camera from "../components/svg/Camera";
 import Img from "../image1.jpg";
+import { storage, db, auth } from "../firebase";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
   const [img, setImg] = useState("");
-  console.log(img)
-  return (
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+      if (docSnap.exists) {
+        setUser(docSnap.data());
+      }
+    });
+
+    if (img) {
+      const uploadImg = async () => {
+        const imgRef = ref(
+          storage,
+          `avatar/${new Date().getTime()} - ${img.name}`
+        );
+        try {
+          const snap = await uploadBytes(imgRef, img);
+          const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
+
+          await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: url,
+            avatarPath: snap.ref.fullPath,
+          });
+          console.log(url);
+          setImg("");
+        } catch (err) {
+          console.log(err.message);
+        }
+      };
+      uploadImg();
+    }
+  }, [img]);
+  return user ? (
     <section>
       <div className="profile_container">
         <div className="img_container">
-          <img src={Img} alt="avatar" />
+          <img src={user.avatar || Img} alt="avatar" />
           <div className="overlay">
             <div>
               <label htmlFor="photo">
@@ -26,14 +60,14 @@ const Profile = () => {
           </div>
         </div>
         <div className="text_container">
-          <h3>User name</h3>
-          <p>User email</p>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
           <hr />
-          <small>Joined on: ...</small>
+          <small>Joined on: {user.createdAt.toDate().toDateString()}</small>
         </div>
       </div>
     </section>
-  );
+  ) : null;
 };
 
 export default Profile;
