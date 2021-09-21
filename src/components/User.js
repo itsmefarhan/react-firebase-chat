8cc45640372694edf8d927db382ceeb3d1e1e153
Farhan Farooq <farhan.farooq@live.com>
2021-09-22 01:37:27 +0500

    select user

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Img from "../image1.jpg";
 
-const User = ({ user }) => {
+const User = ({ user, selectUser }) => {
   return (
-    <div className="user_wrapper">
+    <div className="user_wrapper" onClick={() => selectUser(user)}>
       <div className="user_info">
         <div className="user_detail">
           <img src={user.avatar || Img} alt="avatar" className="avatar" />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import User from "../components/User";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
+  const [chat, setChat] = useState("");
 
   useEffect(() => {
     const usersRef = collection(db, "users");
@@ -20,13 +21,26 @@ const Home = () => {
     });
     return () => unsub();
   }, []);
-  console.log(users);
+
+  const selectUser = (user) => {
+    setChat(user);
+    console.log(user);
+  };
   return (
     <div className="home_container">
       <div className="users_container">
         {users.map((user) => (
-          <User key={user.uid} user={user} />
+          <User key={user.uid} user={user} selectUser={selectUser} />
         ))}
+      </div>
+      <div className="messages_container">
+        {chat ? (
+          <div className="messages_user">
+            <h3>{chat.name}</h3>
+          </div>
+        ) : (
+          <h3 className="no_conv">Select a user to start conversation</h3>
+        )}
       </div>
     </div>
   );
