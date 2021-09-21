b5ca59e2a8ed7b2ff91b39846c0bb29c734467ea
Farhan Farooq <farhan.farooq@live.com>
2021-09-22 02:54:27 +0500

    send message

---
+++ src/components/MessageForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Attachment from "./svg/Attachment";
+
+const MessageForm = ({ handleSubmit, text, setText, setImg }) => {
+  return (
+    <form className="message_form" onSubmit={handleSubmit}>
+      <label htmlFor="img">
+        <Attachment />
+      </label>
+      <input
+        onChange={(e) => setImg(e.target.files[0])}
+        type="file"
+        id="img"
+        accept="image/*"
+        style={{ display: "none" }}
+      />
+      <div>
+        <input
+          type="text"
+          placeholder="Enter message"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div>
+        <button className="btn">Send</button>
+      </div>
+    </form>
+  );
+};
+
+export default MessageForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { db, auth } from "../firebase";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db, auth, storage } from "../firebase";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  addDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import User from "../components/User";
+import MessageForm from "../components/MessageForm";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
   const [chat, setChat] = useState("");
+  const [text, setText] = useState("");
+  const [img, setImg] = useState("");
+
+  const user1 = auth.currentUser.uid;
 
   useEffect(() => {
     const usersRef = collection(db, "users");
     // create query object
-    const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+    const q = query(usersRef, where("uid", "not-in", [user1]));
     // execute query
     const unsub = onSnapshot(q, (querySnapshot) => {
       let users = [];
@@ -26,6 +39,34 @@ const Home = () => {
     setChat(user);
     console.log(user);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user2 = chat.uid;
+
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+
+    let url;
+    if (img) {
+      const imgRef = ref(
+        storage,
+        `images/${new Date().getTime()} - ${img.name}`
+      );
+      const snap = await uploadBytes(imgRef, img);
+      const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath));
+      url = dlUrl;
+    }
+
+    await addDoc(collection(db, "messages", id, "chat"), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || "",
+    });
+    setText("");
+  };
   return (
     <div className="home_container">
       <div className="users_container">
@@ -35,9 +76,17 @@ const Home = () => {
       </div>
       <div className="messages_container">
         {chat ? (
-          <div className="messages_user">
-            <h3>{chat.name}</h3>
-          </div>
+          <>
+            <div className="messages_user">
+              <h3>{chat.name}</h3>
+            </div>
+            <MessageForm
+              handleSubmit={handleSubmit}
+              text={text}
+              setText={setText}
+              setImg={setImg}
+            />
+          </>
         ) : (
           <h3 className="no_conv">Select a user to start conversation</h3>
         )}
