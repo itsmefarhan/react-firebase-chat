84ce145b08239f28743a20d117e3f89eb197a88b
Farhan Farooq <farhan.farooq@live.com>
2021-09-23 02:36:20 +0500

    responsiveness

--- src/components/User.js
+++ src/components/User.js
@@ -16,29 +16,41 @@ const User = ({ user1, user, selectUser, chat }) => {
   }, []);
 
   return (
-    <div
-      className={`user_wrapper ${chat.name === user.name && "selected_user"}`}
-      onClick={() => selectUser(user)}
-    >
-      <div className="user_info">
-        <div className="user_detail">
-          <img src={user.avatar || Img} alt="avatar" className="avatar" />
-          <h4>{user.name}</h4>
-          {data?.from !== user1 && data?.unread && (
-            <small className="unread">New</small>
-          )}
+    <>
+      <div
+        className={`user_wrapper ${chat.name === user.name && "selected_user"}`}
+        onClick={() => selectUser(user)}
+      >
+        <div className="user_info">
+          <div className="user_detail">
+            <img src={user.avatar || Img} alt="avatar" className="avatar" />
+            <h4>{user.name}</h4>
+            {data?.from !== user1 && data?.unread && (
+              <small className="unread">New</small>
+            )}
+          </div>
+          <div
+            className={`user_status ${user.isOnline ? "online" : "offline"}`}
+          ></div>
         </div>
-        <div
-          className={`user_status ${user.isOnline ? "online" : "offline"}`}
-        ></div>
+        {data && (
+          <p className="truncate">
+            <strong>{data.from === user1 ? "Me:" : null}</strong>
+            {data.text}
+          </p>
+        )}
       </div>
-      {data && (
-        <p className="truncate">
-          <strong>{data.from === user1 ? "Me:" : null}</strong>
-          {data.text}
-        </p>
-      )}
-    </div>
+      <div
+        onClick={() => selectUser(user)}
+        className={`sm_container ${chat.name === user.name && "selected_user"}`}
+      >
+        <img
+          src={user.avatar || Img}
+          alt="avatar"
+          className="avatar sm_screen"
+        />
+      </div>
+    </>
   );
 };
 
